8f7c744594dda21a5fb351d7b06a60587c61e5fd
emircalife <emircalife@gmail.com>
2022-06-10 18:42:51 -0300

    Perfil de teste criado

---
+++ src/main/java/com/calife/helpdesk/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.calife.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.calife.helpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	@Autowired
+	private DBService dbservice;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbservice.instanciaDB();
+	}
+}
---
+++ src/main/java/com/calife/helpdesk/services/DBService.java
@@ -0,0 +1,42 @@
+package com.calife.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.calife.helpdesk.domain.Chamado;
+import com.calife.helpdesk.domain.Cliente;
+import com.calife.helpdesk.domain.Tecnico;
+import com.calife.helpdesk.domain.enums.Perfil;
+import com.calife.helpdesk.domain.enums.Prioridade;
+import com.calife.helpdesk.domain.enums.Status;
+import com.calife.helpdesk.repositories.ChamadoRepository;
+import com.calife.helpdesk.repositories.ClienteRepository;
+import com.calife.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		Tecnico tec1 = new Tecnico(null, "Emir Calife", "02159070432", "emircalife@gmail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "16805083001", "torvalds@mail.com", "123");
+		
+		Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(ch1));
+	}
+}
