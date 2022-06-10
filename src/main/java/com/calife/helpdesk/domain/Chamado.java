e5e477e414162c50976242653d588c187b8d85b4
emircalife <emircalife@gmail.com>
2022-06-10 18:29:18 -0300

    Carga inicial da base de dados

--- src/main/java/com/calife/helpdesk/domain/Chamado.java
+++ src/main/java/com/calife/helpdesk/domain/Chamado.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -18,6 +20,7 @@ public class Chamado implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	
 	@JsonFormat(pattern = "dd/MM/yyyy")
