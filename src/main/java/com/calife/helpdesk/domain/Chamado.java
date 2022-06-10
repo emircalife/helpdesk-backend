ee2914d9de969949b06cf9b13a37e743860732ad
emircalife <emircalife@gmail.com>
2022-06-10 14:48:19 -0300

    Anotacoes JPA para classes de dominio

--- src/main/java/com/calife/helpdesk/domain/Chamado.java
+++ src/main/java/com/calife/helpdesk/domain/Chamado.java
@@ -1,22 +1,44 @@
 package com.calife.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import com.calife.helpdesk.domain.enums.Prioridade;
 import com.calife.helpdesk.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
 	protected Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataFechamento;
+	
 	private Prioridade  prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
 	public Chamado() {
 		super();
 	}
