9d2ede20b5c7850e3d7e7b6b51ab1a1d90159093
emircalife <emircalife@gmail.com>
2022-06-11 16:10:26 -0300

    Endpoint create para Tecnico

--- src/main/java/com/calife/helpdesk/domain/Tecnico.java
+++ src/main/java/com/calife/helpdesk/domain/Tecnico.java
@@ -2,10 +2,12 @@ package com.calife.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.calife.helpdesk.domain.dtos.TecnicoDTO;
 import com.calife.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +29,17 @@ public class Tecnico extends Pessoa{
 		addPerfil(Perfil.TECNICO);
 	}
 
+	public Tecnico(TecnicoDTO tecnico) {
+		super();
+		this.id = tecnico.getId();
+		this.nome = tecnico.getNome();
+		this.cpf = tecnico.getCpf();
+		this.email = tecnico.getEmail();
+		this.senha = tecnico.getSenha();
+		this.perfis = tecnico.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = tecnico.getDataCriacao();
+	}
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
