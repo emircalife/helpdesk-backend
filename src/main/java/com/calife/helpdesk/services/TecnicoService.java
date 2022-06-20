e33a589234d00902814cb1819bc54a7dc60266e9
emircalife <emircalife@gmail.com>
2022-06-20 14:15:22 -0300

    Delete para tRécnico implementado

--- src/main/java/com/calife/helpdesk/services/TecnicoService.java
+++ src/main/java/com/calife/helpdesk/services/TecnicoService.java
@@ -48,6 +48,16 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+
+
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui  ordens de serviço e não pode ser deletado");
+		}
+		
+		repository.deleteById(id);
+	}
 	
 	private void validaPorCPFEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -60,4 +70,5 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("Email já cadastrado no sistema");
 		}
 	}
+
 }
