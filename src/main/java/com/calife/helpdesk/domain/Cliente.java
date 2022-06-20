200fa9d2bb447a48eff4500872076e23dbaee59e
emircalife <emircalife@gmail.com>
2022-06-20 14:34:54 -0300

    CRUD de Clientes criado

--- src/main/java/com/calife/helpdesk/domain/Cliente.java
+++ src/main/java/com/calife/helpdesk/domain/Cliente.java
@@ -2,10 +2,12 @@ package com.calife.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.calife.helpdesk.domain.dtos.ClienteDTO;
 import com.calife.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +29,17 @@ public class Cliente extends Pessoa{
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Cliente(ClienteDTO cliente) {
+		super();
+		this.id = cliente.getId();
+		this.nome = cliente.getNome();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.senha = cliente.getSenha();
+		this.perfis = cliente.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = cliente.getDataCriacao();
+	}
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
---
+++ src/main/java/com/calife/helpdesk/domain/dtos/ClienteDTO.java
@@ -0,0 +1,105 @@
+package com.calife.helpdesk.domain.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotNull;
+
+import com.calife.helpdesk.domain.Cliente;
+import com.calife.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	protected Integer id;
+	@NotNull(message = "Campo NOME é requerido")
+	protected String nome;
+	@NotNull(message = "Campo CPF é requerido")
+	protected String cpf;
+	@NotNull(message = "Campo EMAIL é requerido")
+	protected String email;
+	@NotNull(message = "Campo SENHA é requerido")
+	protected String senha;
+	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataCriacao = LocalDate.now();
+
+	public ClienteDTO() {
+		super();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public ClienteDTO(Cliente oldObj) {
+		super();
+		this.id = oldObj.getId();
+		this.nome = oldObj.getNome();
+		this.cpf = oldObj.getCpf();
+		this.email = oldObj.getEmail();
+		this.senha = oldObj.getSenha();
+		this.perfis = oldObj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = oldObj.getDataCriacao();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnaum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfis(Perfil perfis) {
+		this.perfis.add(perfis.getCodigo());
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+}
