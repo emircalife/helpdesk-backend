7125fe36dda7e9847f2b026ae81600e71f693f59
emircalife <emircalife@gmail.com>
2022-06-22 12:21:50 -0300

    FindById para Chamado

---
+++ src/main/java/com/calife/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.calife.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.calife.helpdesk.domain.Chamado;
+import com.calife.helpdesk.repositories.ChamadoRepository;
+import com.calife.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não enocontrado ID: " + id));
+	}
+}
